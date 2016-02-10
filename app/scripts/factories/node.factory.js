e1374f1b5744f36c7fadf4fe7294c3cb2084fc8b
Tom Calvo <calvo.tom@gmail.com>
2016-02-10 11:31:48 +0100

    remove comments

--- app/scripts/factories/node.factory.js
+++ app/scripts/factories/node.factory.js
@@ -1,22 +1,9 @@
 'use strict';
-angular.module('bitbloqOffline')
-    // I provide an injectable (and exteded) version of the underscore / lodash lib.
-    .factory('nodeRemote', function() {
-        // Get a local handle on the global lodash reference.
-        // Return the [formerly global] reference so that it can be injected into other aspects of the AngularJS application.
-        return require('remote');
-    });
-angular.module('bitbloqOffline')
-    // I provide an injectable (and exteded) version of the underscore / lodash lib.
-    .factory('nodeDialog', function(nodeRemote) {
-        // Get a local handle on the global lodash reference.
-        // Return the [formerly global] reference so that it can be injected into other aspects of the AngularJS application.
-        return nodeRemote.require('dialog');
-    });
-angular.module('bitbloqOffline')
-    // I provide an injectable (and exteded) version of the underscore / lodash lib.
-    .factory('nodeFs', function() {
-        // Get a local handle on the global lodash reference.
-        // Return the [formerly global] reference so that it can be injected into other aspects of the AngularJS application.
-        return require('fs');
-    });
+
+angular.module('bitbloqOffline').factory('nodeRemote', function() {
+    return require('remote');
+}).factory('nodeDialog', function(nodeRemote) {
+    return nodeRemote.require('dialog');
+}).factory('nodeFs', function() {
+    return require('fs');
+});
