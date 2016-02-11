bd9993b1f6a7a42769c2727fd36d30490c0c0957
FdomBQ <fernando.delolmo@bq.com>
2016-02-11 16:35:07 +0100

    Deleted app from res prebuilt.

--- res/windows64-prebuilt/resources/app/app/scripts/factories/node.factory.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('bitbloqOffline').factory('nodeRemote', function() {
-    return require('remote');
-}).factory('nodeDialog', function(nodeRemote) {
-    return nodeRemote.require('dialog');
-}).factory('nodeFs', function() {
-    return require('fs');
-});
