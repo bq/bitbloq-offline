7613abd5211350f211b521700a0b4fe82e776465
FdomBQ <fernando.delolmo@bq.com>
2016-02-16 15:46:53 +0100

    changed the file where we get the version from bower to package

--- app/scripts/services/common.js
+++ app/scripts/services/common.js
@@ -21,7 +21,7 @@ angular.module('bitbloqOffline')
 
     exports.bloqsSchemas = JSON.parse(fs.readFileSync(exports.appPath + '/bower_components/bloqs/dist/bloqsmap.json', 'utf8'));
     exports.hardware = JSON.parse(fs.readFileSync(exports.appPath + '/app/res/hw.json', 'utf8'));
-    exports.version = JSON.parse(fs.readFileSync('bower.json', 'utf8')).version;
+    exports.version = JSON.parse(fs.readFileSync(exports.appPath + '/package.json', 'utf8')).version;
     console.log(exports.bloqsSchemas);
     exports.translate = $filter('translate');
     exports.translateTo = function(lang) {
