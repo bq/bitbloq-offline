72ebfbf4917883227efb60287d90caf4e7e02903
Dieguin <diego.segura@bq.com>
2016-02-19 13:45:33 +0100

    production settings on main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ app.on('ready', function() {
   mainWindow.show();
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
