c8d704ccd4912cdc189f786011b4e7f1d3ec7cc0
JorgeGarciaIrazabal <jorge.garcia@bq.com>
2016-03-28 10:37:11 +0200

    fixed drivers installer win32

--- res/windows32-prebuilt/drivers/install_drivers.bat
+++ res/windows32-prebuilt/drivers/install_drivers.bat
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-
 IF EXIST "%PROGRAMFILES(X86)%" (
-data/bqZowi_x64.exe /lm
+"data/bqZowi_x64.exe" /lm
 ) ELSE  (
-data/bqZowi_x86.exe /lm
+"data/bqZowi_x86.exe" /lm
 )
-
 IF EXIST "%PROGRAMFILES(X86)%" (
-data/CP210xVCPInstaller_x64.exe /lm
+"data/CP210xVCPInstaller_x64.exe" /lm
 ) ELSE  (
-data/CP210xVCPInstaller_x86.exe /lm
+"data/CP210xVCPInstaller_x86.exe" /lm
 )
-
 IF EXIST "%PROGRAMFILES(X86)%" (
-data/dpinst64.exe /lm
+"data/dpinst64.exe" /lm
 ) ELSE  (
-data/dpinst32.exe /lm
+"data/dpinst32.exe" /lm
 )
-
-"data/CDM v2.10.00 WHQL Certified.exe"
+"data/CDM v2.10.00 WHQL Certified.exe" /lm
